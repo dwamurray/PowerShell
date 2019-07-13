c7f47fb9cd205c18e8bb9761ea45a78e86d69140
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-07-13 21:48:36 +0100

    Update Gather.ps1

--- Gather.ps1
+++ Gather.ps1
@@ -1,23 +1,3 @@
-<#
-Created:	 2013-01-08
-Version:	 1.0
-Author       Mikael Nystrom and Johan Arwidmark       
-Homepage:    http://www.deploymentartist.com
-
-Disclaimer:
-This script is provided "AS IS" with no warranties, confers no rights and 
-is not supported by the authors or DeploymentArtist.
-
-Author - Mikael Nystrom
-    Twitter: @mikael_nystrom
-    Blog   : http://deploymentbunny.com
-
-Author - Johan Arwidmark
-    Twitter: @jarwidmark
-    Blog   : http://deploymentresearch.com
-#>
-
-
 # Check for elevation
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
     [Security.Principal.WindowsBuiltInRole] "Administrator"))
