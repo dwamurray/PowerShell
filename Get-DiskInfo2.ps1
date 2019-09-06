00c366f404c0673733e029614dbfe9cf333278c0
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-09-06 11:31:04 +0100

    Rename listing 31-4.txt to Get-DiskInfo2.ps1

--- Get-DiskInfo2.ps1
+++ Get-DiskInfo2.ps1
@@ -39,7 +39,7 @@ function Get-DiskInfo {
                    Danger=$danger}
         Write-Debug "Created hash table  will create object next"   #A
         
-        $obj = New-Object –TypeName PSObject –Property $props
+        $obj = New-Object â€“TypeName PSObject â€“Property $props
         Write-Output $obj
       }
     }
