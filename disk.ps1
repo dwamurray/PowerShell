aa05c078ddd550c3ef1f31a7e403832528cdd787
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-09-06 11:35:59 +0100

    Delete disk.ps1

--- disk.ps1
+++
@@ -1,8 +0,0 @@
-Param(                      #A
-	[string]$computerName, #A
-	[int]$driveType = 3    #A
-)
-Get-WmiObject –Class Win32_LogicalDisk –Filter "drivetype=$driveType"  #B
-[CA ]         –ComputerName $computerName |   #C
-Select-Object –Property DeviceID,
-@{Name='ComputerName'; Expression={$_.__SERVER}}, Size, FreeSpace
