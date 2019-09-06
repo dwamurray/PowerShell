70e99bff81334e22f525bbbaf009905c3bc66251
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-09-06 11:42:51 +0100

    Update and rename Untitled5.ps1 to logicaldisk.ps1

--- Untitled5.ps1
+++
@@ -1,4 +0,0 @@
-﻿Get-WmiObject -Class Win32_logicaldisk |
-where {$_.DriveType -eq 3} |
-foreach {$($_.DeviceID + "," + ($_.FreeSpace/ 1MB -as [int]) + "MB," + ($_.Size/ 1MB -as [int])`
-+ "MB," + $(($_.Size/ 1MB -as [int]) - ($_.FreeSpace/ 1MB -as [int])) + "MB," +  $_.VolumeName)}
