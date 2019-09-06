6f821899266beea20758a5b4aee74a03a1611b8b
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-09-06 11:57:49 +0100

    Update and rename listing 21-2.txt to info.ps1

--- info.ps1
+++ info.ps1
@@ -1,4 +1,4 @@
-﻿$os = Get-WmiObject –class Win32_OperatingSystem –comp localhost
+$os = Get-WmiObject –class Win32_OperatingSystem –comp localhost
 $cs = Get-WmiObject –class Win32_ComputerSystem –comp localhost
 $bios = Get-WmiObject –class Win32_BIOS –comp localhost
 $proc = Get-WmiObject –class Win32_Processor –comp localhost | 
