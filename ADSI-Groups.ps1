9a563d3c5e0851f3c5f469ab4b12e7b640197225
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-09-06 11:55:05 +0100

    Update ADSI-Groups.ps1

--- ADSI-Groups.ps1
+++ ADSI-Groups.ps1
@@ -7,8 +7,10 @@ foreach (
 $Group in $groups
 ) {
 $Group | out-File $($folder + $server + "\" +  $server + "-Groups.csv") -Append -Encoding ascii
-$members=$([ADSI]"WinNT://$Server/$Group,group").psbase.Invoke('Members') | foreach { $_.GetType().InvokeMember('ADspath', 'GetProperty', $null, $_, $null).Replace('WinNT://', '') }
-if (
+$members=$([ADSI]"WinNT://$Server/$Group,group").psbase.Invoke('Members') | 
+foreach { 
+$_.GetType().InvokeMember('ADspath', 'GetProperty', $null, $_, $null).Replace('WinNT://', '') 
+} if (
 $members -is [system.array]
 ) {
 foreach (
