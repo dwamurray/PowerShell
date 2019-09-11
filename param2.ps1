69652053c387d2e317e44767be63b0b3b2f2d0f1
David Murray <15840416+dwamurray@users.noreply.github.com>
2019-09-11 09:47:32 +0100

    Create param2.ps1

---
+++ param2.ps1
@@ -0,0 +1,28 @@
+function Get-ServerInfo {
+param (
+$computername = 'localhost'
+)
+
+$os = Get-WmiObject Win32_OperatingSystem -computer $computername
+
+$disk = Get-WmiObject Win32_LogicalDisk -filter "DeviceID='C:'" `
+-computer $computername
+
+$obj = New-Object -TypeName PSObject
+
+$obj | Add-Member -MemberType NoteProperty `
+-Name ComputerName -Value $computername
+
+$obj | Add-Member -MemberType NoteProperty `
+-Name BuildNumber -Value ($os.BuildNumber)
+
+$obj | Add-Member -MemberType NoteProperty `
+-Name SPVersion -Value ($os.ServicePackMajorVersion)
+
+$obj | Add-Member -MemberType NoteProperty `
+-Name SysDriveFree -Value ($disk.free / 1MB -as [int])
+
+Write-Output $obj
+}
+
+Get-ServerInfo 2008member | Format-Table -auto
